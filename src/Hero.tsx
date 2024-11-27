ac9a594fa79f658e3f616ef70420dcb65d603227
Mel Ubalde <melkisedek222@gmail.com>
2024-11-27 22:27:41 +0800

    Update Hero.tsx

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
                         <AvatarFallback>CN</AvatarFallback>
                     </Avatar>
                     <h2 className="scroll-m-20 text-center md:text-left text-white text-2xl sm:text-3xl lg:text-3xl xl:text-5xl tracking-tight">
-                        My name is <span className="font-semibold text-amber-400">Melkisedek Ubalde</span>, I’m a Junior Full-Stack Developer working and living in Philippines
+                        My name is <span className="font-semibold text-amber-400">Melkisedek Ubalde</span>, I’m a Full-Stack Developer working and living in Philippines
                     </h2>
                     <p className="leading-7 text-justify text-white">
                         I specialize in developing end-to-end solutions for web applications, from crafting engaging user experiences to implementing robust backend functionality. With a keen eye for detail and a passion for problem-solving, I thrive in creating seamless digital experiences that delight users.
